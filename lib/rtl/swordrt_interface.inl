7f765e06fb9ac3ba34dbbbadec03a9e9c9eebf99
Simone Atzeni <simone.at@gmail.com>
2016-10-13 17:18:46 -0600

    New method for linear programming

--- lib/rtl/swordrt_interface.inl
+++ lib/rtl/swordrt_interface.inl
@@ -1,3 +1,6 @@
+
+// UTIL
+
 int __swordomp_get_status() {
 	return __swordomp_status__;
 }
@@ -9,6 +12,8 @@ void __swordomp_status_inc() {
 void __swordomp_status_dec() {
 	__swordomp_status__--;
 }
+
+// UTIL
 
 // READS
 void __swordomp_read1(void *addr, uint64_t hash) {
@@ -30,9 +35,9 @@ void __swordomp_read8(void *addr, uint64_t hash) {
 		return;
 
 	// binAccesses = insert(binAccesses, hash, access, 1, size8, unsafe_read);
-	std::unordered_map<uint64_t, node>::const_iterator item = binAccesses.find(hash);
+	std::unordered_map<uint64_t, AccessInfo>::const_iterator item = binAccesses.find(hash);
 	if(item == binAccesses.end()) {
-		binAccesses.insert(std::make_pair(hash, node(access, 1, size8, unsafe_read)));
+		binAccesses.insert(std::make_pair(hash, AccessInfo(access, 1, size8, unsafe_read)));
 	} else {
 		if(access < item->second.address) {
 			binAccesses[hash].address = access;
@@ -64,9 +69,9 @@ void __swordomp_write8(void *addr, uint64_t hash) {
 		return;
 
 	// binAccesses = insert(binAccesses, hash, access, 1, size8, unsafe_write);
-	std::unordered_map<uint64_t, node>::const_iterator item = binAccesses.find(hash);
+	std::unordered_map<uint64_t, AccessInfo>::const_iterator item = binAccesses.find(hash);
 	if(item == binAccesses.end()) {
-		binAccesses.insert(std::make_pair(hash, node(access, 1, size8, unsafe_write)));
+		binAccesses.insert(std::make_pair(hash, AccessInfo(access, 1, size8, unsafe_write)));
 	} else {
 		if(access < item->second.address) {
 			binAccesses[hash].address = access;
@@ -82,111 +87,89 @@ void __swordomp_write8(void *addr, uint64_t hash) {
 
 // LOAD
 void __swordomp_atomic8_load(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size1, atomic_read);
 }
 
 void __swordomp_atomic16_load(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size2, atomic_read);
 }
 
 void __swordomp_atomic32_load(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size4, atomic_read);
 }
 
 void __swordomp_atomic64_load(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size8, atomic_read);
 }
 
 void __swordomp_atomic128_load(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size16, atomic_read);
 }
 // LOAD
 
 // STORE
 void __swordomp_atomic8_store(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size1, atomic_write);
 }
 
 void __swordomp_atomic16_store(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size2, atomic_write);
 }
 
 void __swordomp_atomic32_store(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size4, atomic_write);
 }
 
 void __swordomp_atomic64_store(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size8, atomic_write);
 }
 
 void __swordomp_atomic128_store(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size16, atomic_write);
 }
 // STORE
 
 // ADD
 void __swordomp_atomic8_fetch_add(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size1, atomic_write);
 }
 
 void __swordomp_atomic16_fetch_add(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size2, atomic_write);
 }
 
 void __swordomp_atomic32_fetch_add(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size4, atomic_write);
 }
 
 void __swordomp_atomic64_fetch_add(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size8, atomic_write);
 }
 
 void __swordomp_atomic128_fetch_add(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size16, atomic_write);
 }
 // ADD
 
 // SUB
 void __swordomp_atomic8_fetch_sub(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size1, atomic_write);
 }
 
 void __swordomp_atomic16_fetch_sub(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size2, atomic_write);
 }
 
 void __swordomp_atomic32_fetch_sub(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size4, atomic_write);
 }
 
 void __swordomp_atomic64_fetch_sub(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size8, atomic_write);
 }
 
 void __swordomp_atomic128_fetch_sub(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size16, atomic_write);
 }
 // SUB
 
 // COMPARE EXCHANGE
 void __swordomp_atomic8_compare_exchange_val(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size1, atomic_write);
 }
 
 void __swordomp_atomic16_compare_exchange_val(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size2, atomic_write);
 }
 
 void __swordomp_atomic32_compare_exchange_val(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size4, atomic_write);
+	
 }
 
 void __swordomp_atomic64_compare_exchange_val(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size8, atomic_write);
+	
 }
 
 void __swordomp_atomic128_compare_exchange_val(void *addr, uint64_t hash) {
-	swordRT->CheckMemoryAccess((((size_t) addr)), CALLERPC, size16, atomic_write);
+	
 }
 // COMPARE EXCHANGE
 
--- lib/rtl/swordrt_rtl.cc
+++ lib/rtl/swordrt_rtl.cc
@@ -12,10 +12,6 @@
 
 #include "swordrt_rtl.h"
 
-SwordRT *swordRT;
-
-// Class SwordRT
-
 #if defined(TLS) || defined(NOTLS)
 #define GET_STACK	 										\
 		pthread_t self = pthread_self(); 					\
@@ -31,161 +27,6 @@ SwordRT *swordRT;
 		pthread_attr_getstack(&attr, (void **) &threadInfo[tid - 1].stack, &threadInfo[tid - 1].stacksize);	\
 		pthread_attr_destroy(&attr);
 #endif
-
-SwordRT::SwordRT() {}
-
-SwordRT::~SwordRT() {}
-
-inline void SwordRT::ReportRace(size_t access, size_t pc, uint64_t tid, AccessSize access_size, AccessType access_type, const char *nutex_name) {
-	// Will call a class that executes llvm-symbolizer at the end of each parallel region,
-	// here we just keep filling up the file that holds all the executable/addresses
-	// We also put the address of a parallel region so we know the parallel region where the
-	// access belongs to
-	reported_races.insert(pc);
-	DEBUG(std::cout, "RACE[" << std::hex << access << "] - [" << (void *) pc << "]");
-	DEBUG(std::cerr, "RACE[" << (void *) pc << "]");
-}
-
-void SwordRT::clear() {
-	// DEBUG(std::cout, "Reset filters!");
-	reported_races.clear();
-	CLEAR_FILTER(unsafe_read);
-	CLEAR_FILTER(unsafe_write);
-	CLEAR_FILTER(mutex_read);
-	CLEAR_FILTER(mutex_write);
-	CLEAR_FILTER(atomic_read);
-	CLEAR_FILTER(atomic_write);
-}
-
-void ALWAYS_INLINE SwordRT::CheckMemoryAccess(size_t access, size_t pc, AccessSize access_size, AccessType access_type, const char *nutex_name) {
-	bool conflict = false;
-	AccessType conflict_type = none;
-
-	if(reported_races.probably_contains(pc, hash_value_pc))
-		return;
-
-	// Return if variable belong to thread stack (is local)
-#if defined(TLS) || defined(NOTLS)
-	if((access >= (size_t) stack) &&
-			(access < (size_t) stack + stacksize))
-		return;
-#else
-	if((access >= (size_t) threadInfo[tid - 1].stack) &&
-			(access < (size_t) threadInfo[tid - 1].stack + threadInfo[tid - 1].stacksize))
-		return;
-#endif
-
-	switch(access_type) {
-	case unsafe_read:
-		if(CONTAINS(access, unsafe_write, hash_values)) {
-			conflict = true;
-			conflict_type = unsafe_write;
-		} else if(CONTAINS_HASH(hash_values, access, atomic_write)) {
-			conflict = true;
-			conflict_type = atomic_write;
-		} else if(CONTAINS_HASH(hash_values, access, mutex_write)) {
-			conflict = true;
-			conflict_type = mutex_write;
-		}
-		if(!conflict)
-			INSERT_HASH(hash_values, access, unsafe_read);
-		break;
-	case unsafe_write:
-		if(CONTAINS(access, unsafe_read, hash_values)) {
-			conflict = true;
-			conflict_type = unsafe_read;
-		} else if(CONTAINS_HASH(hash_values, access, unsafe_write)) {
-			conflict = true;
-			conflict_type = unsafe_write;
-		} else if(CONTAINS_HASH(hash_values, access, atomic_read)) {
-			conflict = true;
-			conflict_type = atomic_read;
-		} else if(CONTAINS_HASH(hash_values, access, atomic_write)) {
-			conflict = true;
-			conflict_type = atomic_write;
-		} else if(CONTAINS_HASH(hash_values, access, mutex_read)) {
-			conflict = true;
-			conflict_type = mutex_read;
-		} else if(CONTAINS_HASH(hash_values, access, mutex_write)) {
-			conflict = true;
-			conflict_type = mutex_write;
-		}
-		if(!conflict)
-			INSERT_HASH(hash_values, access, unsafe_write);
-		break;
-	case mutex_read:
-		if(CONTAINS(access, unsafe_write, hash_values)) {
-			conflict = true;
-			conflict_type = unsafe_write;
-		} else if(CONTAINS_HASH(hash_values, access, atomic_write)) {
-			conflict = true;
-			conflict_type = atomic_write;
-		}
-		if(!conflict)
-			INSERT_HASH(hash_values, access, mutex_read);
-		break;
-	case mutex_write:
-		if(CONTAINS(access, unsafe_read, hash_values)) {
-			conflict = true;
-			conflict_type = unsafe_read;
-		} else if(CONTAINS_HASH(hash_values, access, unsafe_write)) {
-			conflict = true;
-			conflict_type = unsafe_write;
-		} else if(CONTAINS_HASH(hash_values, access, atomic_read)) {
-			conflict = true;
-			conflict_type = atomic_read;
-		} else if(CONTAINS_HASH(hash_values, access, atomic_write)) {
-			conflict = true;
-			conflict_type = atomic_write;
-		}
-		if(!conflict)
-			INSERT_HASH(hash_values, access, mutex_write);
-		break;
-	case atomic_read:
-		if(CONTAINS(access, unsafe_write, hash_values)) {
-			conflict = true;
-			conflict_type = unsafe_write;
-		} else if(CONTAINS_HASH(hash_values, access, mutex_write)) {
-			conflict = true;
-			conflict_type = mutex_write;
-		}
-		if(!conflict)
-			INSERT_HASH(hash_values, access, atomic_read);
-		break;
-	case atomic_write:
-		if(CONTAINS(access, unsafe_read, hash_values)) {
-			conflict = true;
-			conflict_type = unsafe_read;
-		} else if(CONTAINS_HASH(hash_values, access, unsafe_write)) {
-			conflict = true;
-			conflict_type = unsafe_write;
-		} else if(CONTAINS_HASH(hash_values, access, mutex_read)) {
-			conflict = true;
-			conflict_type = mutex_read;
-		} else if(CONTAINS_HASH(hash_values, access, mutex_write)) {
-			conflict = true;
-			conflict_type = mutex_write;
-		}
-		if(!conflict)
-			INSERT_HASH(hash_values, access, atomic_write);
-		break;
-	case nutex_read:
-		break;
-	case nutex_write:
-		break;
-	default:
-		return;
-	}
-
-	if(conflict) {
-		// ReportRace(access, pc, tid, access_size, conflict_type, nutex_name);
-		reported_races.insert(pc, hash_value_pc);
-		// DEBUG(std::cout, "RACE[" << std::hex << access << "] - [" << hash_value_pc[0] << "] - [" << tid << "] - [" << (void *) pc << "]");
-		DEBUG(std::cerr, "RACE[" << (void *) pc << "]");
-	}
-}
-
-// Class SwordRT
 
 extern "C" {
 
@@ -211,14 +52,11 @@ static void on_ompt_event_parallel_begin(ompt_task_id_t parent_task_id,
 
 #if defined(TLS) || defined(NOTLS)
 	if(__swordomp_status__ == 0) {
-		swordRT->clear();
-		// clear(binAccesses);
-		// binAccesses = NULL;
 		binAccesses.clear();
 	}
 #else
 	if(threadInfo[tid - 1].__swordomp_status__ == 0) {
-		swordRT->clear();
+		binAccesses.clear();
 	}
 #endif
 }
@@ -246,7 +84,7 @@ static void on_release_critical(ompt_wait_id_t wait_id) {
 static void on_ompt_event_barrier_begin(ompt_parallel_id_t parallel_id,
 		ompt_task_id_t task_id) {
 	// in_order_traversal(binAccesses);
-	//	 for (std::unordered_map<uint64_t, node>::iterator it = binAccesses.begin(); it != binAccesses.end(); ++it)
+	//	 for (std::unordered_map<uint64_t, AccessInfo>::iterator it = binAccesses.begin(); it != binAccesses.end(); ++it)
 	//		 printf("%lu-%lu-0x%lx-%lu-%u\n", tid, it->first, it->second.address, it->second.count, it->second.type);
 }
 
@@ -258,13 +96,6 @@ static void on_ompt_event_barrier_end(ompt_parallel_id_t parallel_id,
 static void ompt_initialize_fn(ompt_function_lookup_t lookup,
 		const char *runtime_version,
 		unsigned int ompt_version) {
-
-	swordRT = new SwordRT();
-
-	if(!swordRT) {
-		std::cerr << "Error initializing the runtime!" << std::endl;
-		exit(-1);
-	}
 
 	DEBUG(std::cout, "OMPT Initizialization: Runtime Version: " << runtime_version << ", OMPT Version: " << ompt_version);
 
