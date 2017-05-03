f68690366e993eea6fdc7525d34fc120174d9c20
Simone Atzeni <simone.at@gmail.com>
2017-05-03 15:28:10 -0600

    Added pointer to file

--- lib/Transforms/Instrumentation/InstrumentParallel.cpp
+++ lib/Transforms/Instrumentation/InstrumentParallel.cpp
@@ -559,7 +559,7 @@ bool InstrumentParallel::runOnFunction(Function &F) {
   // llvm::GlobalVariable *ompStack = NULL;
   // llvm::GlobalVariable *ompStackSize = NULL;
   llvm::GlobalVariable *ompOffset = NULL;
-  // llvm::GlobalVariable *ompDatafile = NULL;
+  llvm::GlobalVariable *ompDatafile = NULL;
 
   Module *M = F.getParent();
   StringRef functionName = F.getName();
@@ -581,7 +581,7 @@ bool InstrumentParallel::runOnFunction(Function &F) {
   // ompStack = M->getNamedGlobal("stack");
   // ompStackSize = M->getNamedGlobal("stacksize");
   ompOffset = M->getNamedGlobal("offset");
-  // ompDatafile = M->getNamedGlobal("datafile");
+  ompDatafile = M->getNamedGlobal("datafile");
 
   if(functionName.compare("main") == 0) {
     IRBuilder<> IRB(M->getContext());
@@ -597,7 +597,7 @@ bool InstrumentParallel::runOnFunction(Function &F) {
     // TLS_DECLARE(ompStack, IRB.getInt8PtrTy(), "stack"); // size_t *
     // TLS_DECLARE(ompStackSize, IRB.getInt64Ty(), "stacksize"); // size_t
     TLS_DECLARE(ompOffset, IRB.getInt64Ty(), "offset"); // size_t
-    // TLS_DECLARE(ompDatafile, IRB.getInt8PtrTy(), "datafile"); // FILE *
+    TLS_DECLARE(ompDatafile, IRB.getInt8PtrTy(), "datafile"); // FILE *
 
     return true;
   }
@@ -625,7 +625,7 @@ bool InstrumentParallel::runOnFunction(Function &F) {
   // TLS_DECLARE_EXTERN(ompStack, IRB.getInt8PtrTy(), "stack"); // size_t *
   // TLS_DECLARE_EXTERN(ompStackSize, IRB.getInt64Ty(), "stacksize"); // size_t
   TLS_DECLARE_EXTERN(ompOffset, IRB.getInt64Ty(), "offset"); // size_t
-  // TLS_DECLARE_EXTERN(ompDatafile, IRB.getInt8PtrTy(), "datafile"); // FILE *
+  TLS_DECLARE_EXTERN(ompDatafile, IRB.getInt8PtrTy(), "datafile"); // FILE *
 
   if(functionName.startswith(".omp")) {
     IF = &F;
